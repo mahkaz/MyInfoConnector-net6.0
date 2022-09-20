ba07b77c3afe6cc8716e190564626a4615f8a83f
mahkaz <marcus.tan.jun.wen@gmail.com>
2022-09-20 19:03:22 +0800

    make standalone

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,10 +3,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("MyInfoConnector")]
-[assembly: AssemblyDescription(".Net Connector for MyInfo")]
-[assembly: InternalsVisibleTo("MyInfoConnector.Tests")]
+[assembly: AssemblyCompany("Government of Singapore")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
+// Allow CI/CD to stamp this
+[assembly: AssemblyInformationalVersion("local-build")]
 
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(false)]
+#if DEBUG
+[assembly: AssemblyProduct("MyInfoConnector (Debug)")]
+[assembly: AssemblyConfiguration("Debug")]
+#else
+    [assembly: AssemblyProduct("MyInfoConnector (Release)")]
+    [assembly: AssemblyConfiguration("Release")]
+#endif
